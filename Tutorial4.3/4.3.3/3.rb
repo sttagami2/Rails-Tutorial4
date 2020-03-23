078aaf13621ab3f2e5d367955dbbad5486d19986
sttagami2 <sttagami2@gmail.com>
2020-03-23 19:24:38 +0900

    [Finish] 4.3.3

---
+++ Tutorial4.3/4.3.3/3.rb
@@ -0,0 +1,3 @@
+# userというハッシュを定義してみてください。このハッシュは３つのキー:name、:email、:password_digestを持っていて、それぞれの値にあなたの名前、あなたのメールアドレス、そして16文字からなるランダムな文字列が代入されています。
+
+user = { name: "Shoto", email: "shoto@gmail.com", password_digest: "abshejdkgndmrjdk" }
